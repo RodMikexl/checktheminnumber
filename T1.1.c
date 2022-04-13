65b5007ffdfcaa5ecb2ee08bcb953a8c14e4b363
Weihong <a1071108631@gmail.com>
2022-04-13 10:21:29 +1000

    enter 10 number then check the min one

---
+++ T1.1.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : 1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void smallestOfThree(int x, int y, int z){
+
+}
+
+int main(){
+
+}
+
