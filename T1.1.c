e03847089551025ac8167ea412bee7bb0aa42c2c
Weihong <a1071108631@gmail.com>
2022-04-13 21:30:53 +1000

    smallest of three numbers

--- T1.1.c
+++ T1.1.c
@@ -1,11 +1,5 @@
 /*
- ============================================================================
- Name        : 1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+
  */
 
 #include <stdio.h>
@@ -22,8 +16,25 @@ void smallestOfThree(int x, int y, int z){
     }
 }
 
+//when the user enter the 5 value, it will show three of the smallest one
 int main(){
     int min[3],n,t;
+    printf("Enter 5 value:");
+    for (int i=0;i<5;i++){
+    scanf("%d",&n);
+    if (i<3) min[i]=n;
+    else{
+        if (n<min[0])
+            {t=n;n=min[0];min[0]=t;}
+        if (n<min[1])
+            {t=n;n=min[1];min[1]=t;}
+        if (n<min[2])
+            {t=n;n=min[2];min[2]=t;}
+    }
+    }
+    printf("the three smallest number is :%d, %d, %d\n", min[0], min[1], min[2]);
+    smallestOfThree(min[0], min[1], min[2]);
+    return 0;
     
 }
 
